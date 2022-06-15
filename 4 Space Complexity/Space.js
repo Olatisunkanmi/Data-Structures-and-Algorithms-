2b9f5a1110d0b004cac2e393901bd775ddd3e9b1
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-06-15 09:42:46 +0100

    Space Complexity

---
+++ 4 Space Complexity/Space.js
@@ -0,0 +1,21 @@
+/**
+ * ^what is space complexity ?
+ * 
+ * ^This is how much more memory use (RAM) do we need as the inputs provided to the Code gets larger unlike,
+ * ^ Time Complexity which is How much runtime do we need as inputs provided to he code gets larger 
+ * 
+ * ! Nte. Both of these complexities still uses Big O Notation
+ * 
+ * 
+ * ^ RUles of Space complexities 
+ * 
+ * ^ ¬¬ Storing values in Variables always takes up memory.
+ * 
+ * ~Most Primities (booleans and Numbers ) take up 0(1)/ Constant Space Complexity
+ * ~        var x = 100 & var x = 200 take up same amount of memory
+ * 
+ * 
+ * ~ Strings, Arrays and Objects takes up 0(n)/ Linear space 
+ *          ~ An array w/ 4 elements takes up twice the memory of an Array w/ 2 elements 
+ * 
+ */
