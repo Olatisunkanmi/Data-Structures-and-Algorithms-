d120e5e51d0d777b815f2d7ccbf6640930362923
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-07 04:23:10 +0100

    Create script.js

---
+++ 5 Algorithms/Sort Algorithms/script.js
@@ -0,0 +1,30 @@
+const sortSelection = (arr) => {
+    let smallest = arr[0];
+    let smallest_index = 0;
+    let newarr = []
+
+        function forLoop (params) {
+            for (let i = 0; i < arr.length; i++) {
+          
+                if  (  arr[i] < smallest ) {
+                    console.log('first')
+                        smallest = arr[i]
+                        smallest_index = i
+                        
+                      
+                }   
+        }
+
+        }      forLoop()
+        return smallest_index
+
+        const sort = () => {
+                smallest =  
+        }
+
+    
+
+}
+
+
+sortSelection([4,2,3 ,4,5,6,7,8,9,10,11,12, 1,13,14,15, 0,16,17,18,19,20] ) 
