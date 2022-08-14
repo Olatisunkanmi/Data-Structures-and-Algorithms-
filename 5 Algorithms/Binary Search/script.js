755884c28c87b78fbba87b9ce087b8a4e17c28f2
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-14 14:02:49 +0100

    Update script.js

--- 5 Algorithms/Binary Search/script.js
+++ 5 Algorithms/Binary Search/script.js
@@ -1,41 +1,39 @@
+const item = 14;
+const arr = [
+	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+	20,
+];
 
+const Search = (arr, item) => {
+	for (let i = 0; i < arr.length; i++) {
+		if (item == arr[i]) {
+			return arr[i];
+		}
+	}
+	return null;
+};
 
-const uSrch = 100
-const SrchArr = [1, 2, 3, 5, 6, 7, 84 ,8, 90, 100, 99, 45, 4];
+Guess(arr, item);
 
-const Guess = () => {
+//^ The Big O Notation of this search Algorithm is O(n) which is linear time
+const Search = (arr, item) => {
+	let low = 0;
+	let high = arr.length - 1;
 
-    for (let i = 0; i < SrchArr.length; i++) {
-        const el= SrchArr[i];
-            if( el === uSrch )  {
-                // console.log(el)
-            }
-    }
-}
+	while (arr[low] <= arr[high]) {
+		let mid = Math.floor((low + high) / 2);
+		let guess = arr[mid];
 
-Guess()
+		if (guess == item) {
+			return arr[mid];
+		} else if (guess > item) {
+			high = mid - 1;
+		} else {
+			low = mid + 1;
+		}
+	}
 
-//^ The Big O Notation of this search Algorithm is O(n) which is linear time.
+	return null;
+};
 
-
-const BinarySearch = (arr, item) => {
-    let low = 0;
-    let high = arr.length - 1;
-    let mid = Math.floor((low + high) / 2 );
-    let guess =  arr[mid];
-    
-    while (arr[low] <= arr[high] ) {
-            if( guess == item ){
-            return mid;
-        }	else if ( guess > item  ){
-            high =  mid - 1 ;
-        }	else {
-            low =  mid + 1 ;
-        }  
-         return null 
-    }
-
-}
-
-
-console.log(BinarySearch([1,2,3 ,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], 33 ) )
+BinarySearch(arr, item);
