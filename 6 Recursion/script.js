7395761058c15acb19a7e5abb571c294d1890e63
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-18 22:48:58 +0100

    Update script.js

--- 6 Recursion/script.js
+++ 6 Recursion/script.js
@@ -7,19 +7,19 @@
 
 // console.log(factorial(5));
 
-// const sumArr = (arr) => {
-// 	if (arr.length == 0) {
-// 		return 1;
-// 	}
-// 	return arr.shift() + sumArr(arr);
-// };
-
 const sumArr = (arr) => {
-	if (arr.length !== 0) {
-		return arr.shift() + sumArr(arr);
-	} else {
+	if (arr.length == 0) {
 		return 1;
 	}
+	return arr.shift() + sumArr(arr);
 };
+
+// const sumArr = (arr) => {
+// 	if (arr.length !== 0) {
+// 		return arr.shift() + sumArr(arr);
+// 	} else {
+// 		return 1;
+// 	}
+// };
 
 console.log(sumArr([2, 3, 4, 5, 6, 7, 8, 9]));
