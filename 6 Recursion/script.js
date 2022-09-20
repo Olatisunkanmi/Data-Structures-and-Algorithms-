8ef26f906a492292cb77fd3edea4236ff0faca65
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-20 17:37:33 +0100

    Update script.js

--- 6 Recursion/script.js
+++ 6 Recursion/script.js
@@ -1,25 +1,54 @@
-// const factorial = (x) => {
-// 	if (x === 0) {
-// 		return 1;
-// 	}
-// 	return x * factorial(x - 1);
-// };
+const Arr = [1, 2, 3, 4, 5, 6, 7, 8, , 9, 34, 43, 56, 76, 233, 545];
+
+const factorial = (x) => {
+	console.log(xs);
+	if (x === 0) {
+		return 1;
+	}
+	return x * factorial(x - 1);
+};
 
 // console.log(factorial(5));
 
-const sumArr = (arr) => {
+const SumArr = (arr) => {
 	if (arr.length == 0) {
 		return 1;
 	}
+	console.log(arr[i]);
 	return arr.shift() + sumArr(arr);
 };
 
-// const sumArr = (arr) => {
-// 	if (arr.length !== 0) {
-// 		return arr.shift() + sumArr(arr);
-// 	} else {
-// 		return 1;
-// 	}
-// };
+// console.log(SumArr(Arr));
 
-console.log(sumArr([2, 3, 4, 5, 6, 7, 8, 9]));
+const sumArr = (arr) => {
+	console.log(arr);
+	if (arr.length !== 0) {
+		return arr.shift() + sumArr(arr);
+	} else {
+		return 0;
+	}
+};
+// console.log(sumArr(Arr));
+
+//  ^ Recursive check for even numbers
+
+const evenArr = (arr) => {
+	let cout = 0;
+	if (arr.length == 0) {
+		return 0;
+		// base case of the function
+	}
+	if (arr.length[i]) {
+	}
+};
+
+//  ^ 	Write  a recursive fnn to count then number of items in a list
+
+const countArr = (arr) => {
+	let count = 0;
+	if (arr.length == 0) {
+		return 0;
+	}
+	// return count + countArr(arr);
+};
+// console.log(countArr(Arr));
